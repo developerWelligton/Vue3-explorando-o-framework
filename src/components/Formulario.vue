fb4189052e237880b5a19c4b3400065ba089a006
Welligton <developerwelligton@gmail.com>
2022-11-09 20:07:16 -0300

    limpando e cronometrando tarefa

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -7,16 +7,16 @@
             <div class="column">
                 <div class="is-flex is-align-items-center is-justify-content-space-between"></div>
                 <section>
-                    <strong>00:00:00</strong>
+                    <strong>{{tempoDecorrido}}</strong>
                 </section>
             </div>
-            <button class="button">
+            <button class="button" @click="iniciar">
                 <span class="icon">
                     <i class="fas fa-play"></i>
                 </span>
                 <span>play</span>
             </button>
-            <button class="button">
+            <button class="button" @click="finalizar">
                 <span class="icon">
                     <i class="fas fa-stop"></i>
                 </span>
@@ -29,6 +29,28 @@
 <script lang="ts"> 
  import { defineComponent} from 'vue';
  export default defineComponent ({
-    name:'Formulario'
+    name:'Formulario',
+    data(){
+        return{
+            tempoEmSegundos:0,
+            cronometro: 0
+        }
+    },
+    computed:{
+        tempoDecorrido() : string{
+            return new Date(this.tempoEmSegundos*1000).toISOString().substr(11,8)
+        }
+    },
+    methods:{
+        iniciar(){
+            //começar a contagem
+             this.cronometro = setInterval(()=> {
+                this.tempoEmSegundos += 1;
+            },1000) 
+        },
+        finalizar(){
+           clearInterval(this.cronometro)
+        }
+    }
  })
 </script>
