27e66feb2516415cd04e31fbdff2d1717327bb5e
Welligton <developerwelligton@gmail.com>
2022-11-09 20:27:21 -0300

    refatorando temporizador

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -4,55 +4,20 @@
             <div class="column is-8" role="form" aria-label="Formulario para criação de uma nova tarefa">
             <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?"/>
             </div>
-            <div class="column">
-                <div class="is-flex is-align-items-center is-justify-content-space-between"></div>
-                <Cronometro :tempoEmSegundos="tempoEmSegundos" />
-            </div>
-            <button class="button" @click="iniciar">
-                <span class="icon">
-                    <i class="fas fa-play"></i>
-                </span>
-                <span>play</span>
-            </button>
-            <button class="button" @click="finalizar">
-                <span class="icon">
-                    <i class="fas fa-stop"></i>
-                </span>
-                <span>stop</span>
-            </button>
+                <div class="column">
+                     <Temporizador/>
+                </div>
         </div>
     </div>
 </template>
 
 <script lang="ts"> 
  import { defineComponent} from 'vue';
- import Cronometro from './Cronometro.vue';
+    import Temporizador from './Temporizador.vue';   
  export default defineComponent ({
     name:'Formulario',
     components:{
-        Cronometro
-    },
-    data(){
-        return{
-            tempoEmSegundos:0,
-            cronometro: 0
-        }
-    },
-    computed:{
-        tempoDecorrido() : string{
-            return new Date(this.tempoEmSegundos*1000).toISOString().substr(11,8)
-        }
-    },
-    methods:{
-        iniciar(){
-            //começar a contagem
-             this.cronometro = setInterval(()=> {
-                this.tempoEmSegundos += 1;
-            },1000) 
-        },
-        finalizar(){
-           clearInterval(this.cronometro)
-        }
-    }
- })
+        Temporizador  
+    } 
+ });
 </script>
